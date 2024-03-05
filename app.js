59078bb0082c981534fdf4b02ed05d2be547171f
Vicente Jr <vjrt.c.o@gmail.com>
2024-03-05 15:05:08 -0300

    Evitando bugs

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!')
+        return;
+    }
+    if (quantidade > (ate - de) + 1) {
+        alert(`Campo "quantidade" deve ser inferior a ${ate - de}. Verifique!`)
+        return;
+    }
+
     let sorteados = [];
     let numero 
 
